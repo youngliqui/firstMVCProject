f976296e5889fd0a1760aa71ffb8595460eb17bb
youngliqui <super.pav-79@yandex.by>
2023-09-22 22:52:00 +0300

    controllers first and second

---
+++ src/main/java/by/youngliqui/firstMVC/controllers/FirstController.java
@@ -0,0 +1,17 @@
+package by.youngliqui.firstMVC.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+}
---
+++ src/main/java/by/youngliqui/firstMVC/controllers/SecondController.java
@@ -0,0 +1,13 @@
+package by.youngliqui.firstMVC.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
+}
