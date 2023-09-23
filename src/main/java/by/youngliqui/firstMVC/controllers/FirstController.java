84a6e4c4ec73c382734557311d12d2dea1094213
youngliqui <super.pav-79@yandex.by>
2023-09-23 10:54:25 +0300

    first request

--- src/main/java/by/youngliqui/firstMVC/controllers/FirstController.java
+++ src/main/java/by/youngliqui/firstMVC/controllers/FirstController.java
@@ -1,17 +1,24 @@
 package by.youngliqui.firstMVC.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello! " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage() {
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("GoodBye! " + name + " " + surname);
         return "first/goodbye";
     }
 }
