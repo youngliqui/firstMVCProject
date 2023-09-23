e3d9a9a40d3ae69e0c5d1ab09dfb4e66c33d84b6
youngliqui <super.pav-79@yandex.by>
2023-09-23 12:40:30 +0300

    simple calculator

--- src/main/java/by/youngliqui/firstMVC/controllers/FirstController.java
+++ src/main/java/by/youngliqui/firstMVC/controllers/FirstController.java
@@ -2,16 +2,19 @@ package by.youngliqui.firstMVC.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
+    public String helloPage(HttpServletRequest request, Model model) {
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
-        System.out.println("Hello! " + name + " " + surname);
+
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
@@ -20,5 +23,28 @@ public class FirstController {
                               @RequestParam(value = "surname", required = false) String surname) {
         System.out.println("GoodBye! " + name + " " + surname);
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a", required = false) int a,
+                                 @RequestParam(value = "b", required = false) int b,
+                                 @RequestParam(value = "action", required = false) String action,
+                                 Model model) {
+
+
+        switch (action) {
+            case "multiplication" -> model.addAttribute("result", a * b);
+            case "addition" -> model.addAttribute("result", a + b);
+            case "subtraction" -> model.addAttribute("result", a - b);
+            case "division" -> {
+                if (b == 0) {
+                    model.addAttribute("result", "Error!");
+                } else {
+                    model.addAttribute("result", a / (double) b);
+                }
+            }
+        }
+
+        return "first/calculator";
     }
 }
