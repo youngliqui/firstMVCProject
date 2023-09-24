b825760734198dd71d65d5e86750bee9bcd6b7c2
youngliqui <super.pav-79@yandex.by>
2023-09-24 17:04:53 +0300

    new person fields

--- src/main/java/by/youngliqui/firstMVC/dao/PersonDAO.java
+++ src/main/java/by/youngliqui/firstMVC/dao/PersonDAO.java
@@ -14,10 +14,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Alex"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Alex", 18, "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 34, "katy@gmail.com"));
     }
 
     public void save(Person person) {
@@ -28,6 +28,8 @@ public class PersonDAO {
     public void update(int id, Person updatedPerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
